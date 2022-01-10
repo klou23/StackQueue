8ba941a6e9b3e704527633efd6c1c4077bc40324
Kevin Lou <58053969+klou23@users.noreply.github.com>
2022-01-10 09:39:15 -0700

    implemented all methods

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,9 +1,40 @@
+import java.util.NoSuchElementException;
+
 public class StackQueue <T>{
 
     Stack<T> s;
+    Stack<T> s2;
 
     public StackQueue() {
         s = new Stack<>();
+    }
+
+    void enqueue(T element){
+        s.push(element);
+    }
+
+    T dequeue(){
+        while(!s.isEmpty()) s2.push(s.pop());
+        if(s2.isEmpty()) throw new NoSuchElementException();
+        T ret = s2.pop();
+        while(!s2.isEmpty()) s.push(s2.pop());
+        return ret;
+    }
+
+    boolean isEmpty(){
+        return s.size() == 0;
+    }
+
+    int size(){
+        return s.size();
+    }
+
+    T peek(){
+        while(!s.isEmpty()) s2.push(s.pop());
+        if(s2.isEmpty()) throw new NoSuchElementException();
+        T ret = s2.peek();
+        while(!s2.isEmpty()) s.push(s2.pop());
+        return ret;
     }
 
 }
